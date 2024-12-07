8ca8ec0afa35a30219ac38ae4c13293a4b798c99
santoshini <santoshinipradhan01@gmail.com>
2024-12-07 11:21:32 +0530

    routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage(){
+    return(
+        <div>
+            this is our about page
+        </div>
+    )
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 
 
-export function ContactPge() {
+export default function ContactPge() {
   return (
     <div>this is your contact page</div>
   )
